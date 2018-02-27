741239985b095940c6dd13b4b9c63a9085c8d8d0
Travis CI User <travis@example.org>
2018-02-27 10:52:51 +0000

    Documentation for rust-lang-nursery/rand@7069151

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"rand/jitter/enum.TimerError.html\" title=\"enum rand::jitter::TimerError\">TimerError</a>",synthetic:false,types:["rand::jitter::TimerError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">stdError</a> for <a class=\"struct\" href=\"rand/struct.Error.html\" title=\"struct rand::Error\">Error</a>",synthetic:false,types:["rand::error::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
